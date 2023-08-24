2db997cf29e0c64bc6e7ab729beb4a57c60dd9e9
lore <sanchezlrt23@gmail.com>
2023-08-24 00:08:06 -0400

    Agregando docs con swagger

--- src/main/java/com/example/pruebaapi/PruebaApiApplication.java
+++ src/main/java/com/example/pruebaapi/PruebaApiApplication.java
@@ -2,12 +2,25 @@ package com.example.pruebaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
 public class PruebaApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PruebaApiApplication.class, args);
+
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("prueba-api")
+						.version("0.01"));
 	}
 
 }
