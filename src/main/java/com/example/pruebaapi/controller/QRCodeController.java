2334fcf6583e492d6fee327cbbc68d3d9dc47f29
lore <sanchezlrt23@gmail.com>
2023-08-24 14:47:28 -0400

    Creando cotrolador y servicio para QRCode

---
+++ src/main/java/com/example/pruebaapi/controller/QRCodeController.java
@@ -0,0 +1,34 @@
+package com.example.pruebaapi.controller;
+
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.pruebaapi.service.QRCodeService;
+
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServletResponse;
+
+@RestController
+public class QRCodeController {
+
+    @Autowired
+    private QRCodeService qrCodeService;
+    
+    @GetMapping("@Service")
+    public void generateQRCode(HttpServletResponse response,
+                               @RequestParam String text,
+                               @RequestParam(defaultValue = "350") int width,
+                               @RequestParam(defaultValue = "350") int height) throws Exception {
+        BufferedImage image = qrCodeService.generateQRCode(text, width, height);
+        ServletOutputStream outputStream = response.getOutputStream();
+        ImageIO.write(image, "png", outputStream);
+        outputStream.flush();
+        outputStream.close();
+    }
+}
